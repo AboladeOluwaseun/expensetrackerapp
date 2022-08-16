eccd1ced93aaad6c6c1f331f6f12eecbce9fcc58
Maxibrain <aboladeoluwaseungodwithus@gmail.com>
2022-08-16 10:19:44 +0100

    login and register styled

--- Components/AuthComponents/Register.js
+++ Components/AuthComponents/Register.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import loginimg from "../../Assets/AuthAssets/Login-bro.png";
 
-const Login = () => {
+const Register = () => {
   return (
     <>
       <div className="h-[100vh]">
@@ -15,16 +15,23 @@ const Login = () => {
           alt="login-illustration"
         />
         <div className="max-w-[85%] sm:max-w-[65%] md:max-w-[50%] lmd:max-w-[35%] xl:max-w-[30%] h-[60%] p-6  absolute top-0 bottom-0 left-0 right-0 m-auto rounded-lg ">
-          <div className="text-start mt-10">
+          <div className="text-start mt-8">
             <h2 className="font-bold text-[1.7rem]">
-              Welcome Back, <br />
-              Kindly <span className="text-violet">Login</span> Now
+              Hi,Welcome. <br />
+              Kindly <span className="text-violet">Register</span> below.
             </h2>
           </div>
 
           <form className="mt-8  ">
             <input
               className="w-full border-darkgrey border-solid border-[1px] h-8 py-5 px-2 rounded-lg focus:outline-none"
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter a desired username"
+            />
+            <input
+              className="w-full border-darkgrey border-solid border-[1px] h-8 py-5 px-2 rounded-lg focus:outline-none mt-6"
               type="email"
               name="email"
               id="email"
@@ -41,13 +48,13 @@ const Login = () => {
               className="block w-full h-10 rounded-lg bg-violet  text-white mt-6"
               type="submit"
             >
-              Login
+              Signup
             </button>
           </form>
           <p className="mt-4">
-            If You're new, Create an account
+            Already have an account?Login
             <span className="text-violet underline">
-              <Link href="/Auth/Register"> here</Link>
+              <Link href="/Auth/Login"> here</Link>
             </span>
           </p>
         </div>
@@ -56,4 +63,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
---
+++ pages/Auth/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "../../Components/AuthComponents/Register";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <Register></Register>
+    </>
+  );
+};
+
+export default RegisterPage;
