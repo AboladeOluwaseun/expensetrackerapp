86b4b372e9f1bcebfc479ee3ec404b6848c919a4
Maxibrain <aboladeoluwaseungodwithus@gmail.com>
2022-09-02 23:00:24 +0100

    ...

--- Components/DashboardComponents/ItemsDisplayAndForm/WelcomeDisplay.js
+++ Components/DashboardComponents/ItemsDisplayAndForm/WelcomeDisplay.js
@@ -4,16 +4,18 @@ import { Paper } from "@mui/material";
 const WelcomeDisplay = ({ windowWidth }) => {
   return (
     <>
-      <Paper>
-        <div className={`py-6 ${windowWidth < 924 ? "mt-32" : "mt-0"} `}>
-          <h4 className={"text-violet text-center  text-[4rem]"}>welcome!</h4>
-          <p className="text-center  mt-4">
-            {windowWidth < 924
-              ? 'click on the "+" button below to add Income'
-              : "fill the form below to add Income/Expense"}
-          </p>
-        </div>
-      </Paper>
+      <div className={`py-6  `}>
+        <Paper>
+          <div>
+            <h4 className={"text-violet text-center  text-[4rem]"}>welcome!</h4>
+            <p className="text-center  mt-4">
+              {windowWidth < 924
+                ? 'click on the "+" button below to add Income'
+                : "fill the form below to add Income/Expense"}
+            </p>
+          </div>
+        </Paper>
+      </div>
     </>
   );
 };
